f48a27ea8d2ed5c72e8441651484b05f1d97e9bd
Akhil Kumar <akhil.se2024@gmail.com>
2024-10-13 03:13:22 +0530

    first commit

---
+++ src/sections/Experience.jsx
@@ -0,0 +1,102 @@
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense, useState } from 'react'
+import { workExperiences } from '../constants'
+import { OrbitControls } from '@react-three/drei'
+import CanvasLoader from '../components/CanvasLoader'
+import Developer from '../components/Developer'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+const Experience = () => {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    useGSAP(()=>{
+        // gsap.from('#workExperience',{
+        //     x:-100,
+        //     opacity:0,
+        //     duration:2,
+        //     scrollTrigger:{
+        //         trigger:"#workExperience",
+        //         start:"top 80%",
+        //         end:"bottom 60%",
+        //         // markers:true,
+        //         scrub:1,
+        //     }
+        // })
+        gsap.from("#projectLeft",{
+            x:-100,
+            opacity:0,
+            duration:2,
+            scrollTrigger:{
+                trigger:"#projectLeft",
+                start:"top 50%",
+                end:"bottom 80%",
+                // markers:true,
+                scrub:1,
+            }
+        })
+        gsap.from("#projectRight",{
+            x:100,
+            opacity:0,
+            // duration:1,
+            stagger:-0.2,
+            scrollTrigger:{
+                trigger:"#projectRight",
+                start:"top 70%",
+                end:"bottom 95%",
+                // markers:true,
+                scrub:1,
+            }
+        });
+    })
+
+
+    const [animationName, setAnimationName] = useState('idle')
+
+
+  return (
+    <section className='c-space my-20'>
+    <div className='w-full text-white-600'>
+        <h3 id='#workExperience' className='head-text'>My Work Experience</h3>
+        <div className='work-container'>
+            <div id='projectLeft' className='work-canvas'>
+                <Canvas>
+                <ambientLight intensity={7}/>
+                <spotLight position={[10,10,10]} angle={0.15} penumbra={1}/>
+                <directionalLight position={[10,10,10]} intensity={1}/>
+                <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} />
+                <Suspense fallback={<CanvasLoader/>}>
+                    <Developer position-y={-3} scale={3} animationName={animationName}
+                    />
+                </Suspense>
+                </Canvas>
+            </div>
+            <div id='projectRight' className='work-content'>
+                <div  className='sm:py-10 py-5 sm:px-5 px-2.5'>
+                    {workExperiences.map(({id,name,pos,icon, duration,title,animation})=>(
+                        <div  key={id} className='work-content_container group' onClick={()=> setAnimationName(animation.toLowerCase())} onPointerOver={()=>setAnimationName(animation.toLowerCase())} onPointerOut={()=>setAnimationName("idle")}>
+                            <div className='flex flex-col h-full justify-start items-center py-2'>
+                            <div className='work-content_logo'>
+                            <img src={icon} alt='logo' className='w-full h-full' />
+                            </div>
+                            <div className='work-content'/>
+                            </div>
+                            <div className='sm:p-5 px-2.5 py-5'>
+                            <p className='font-bold text-white-800'>{name}</p>
+                            <p className='text-sm mb-5'>{pos} -- {duration}</p>
+                            <p className='group-hover:text-white transition ease-in-out duration-500'>{title}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </section>
+  )
+}
+
+export default Experience
