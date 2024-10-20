59ec58583e262fee527cbee52e656805d5fd22d1
Akhil Kumar <akhil.se2024@gmail.com>
2024-10-20 17:06:37 +0530

    Added my github and insta

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -10,13 +10,10 @@ const Footer = () => {
         </div>
         <div className='flex gap-3'>
             <div className='social-icon'>
-                <img src='/assets/github.svg' alt='github' className='w-1/2 h-1/2' />
+                <a href='https://github.com/akhil-k-se' target="_blank" className='social-icon'><img src='/assets/github.svg' alt='github' className='w-1/2 h-1/2' /></a>
             </div>
             <div className='social-icon'>
-                <img src='/assets/twitter.svg' alt='twitter' className='w-1/2 h-1/2' />
-            </div>
-            <div className='social-icon'>
-                <img src='/assets/instagram.svg' alt='instagram' className='w-1/2 h-1/2' />
+                <a href='https://www.instagram.com/akh_il.k/' target="_blank" className='social-icon'><img src='/assets/instagram.svg' alt='instagram' className='w-1/2 h-1/2' /></a>
             </div>
         </div>
         <p className='text-white-500'>© 2024 Akhil, All Rights Reserved</p>
